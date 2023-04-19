a1c2825d35ec515b88fbc37cd7a12cd24e71f32a
Graham Flaspoehler <grahamflas@gmail.com>
2023-04-19 07:23:08 -0400

    Remove icons

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const liInstall = document.getElementById("install-container");
 const buttonInstall = document.getElementById("install-button");
 
-/* Put code here */
-
 /* Only register a service worker if it's supported */
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
@@ -10,30 +8,35 @@ if ("serviceWorker" in navigator) {
 
 
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent the mini-infobar from appearing on mobile.
-  event.preventDefault();
   console.log('👍', 'beforeinstallprompt', event);
+
   // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
+
   // Remove the 'hidden' class from the install button container.
   liInstall.classList.toggle('hidden', false);
 });
 
 buttonInstall.addEventListener('click', async () => {
   console.log('👍', 'butInstall-clicked');
+
   const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
     // The deferred prompt isn't available.
     return;
   }
+
   // Show the install prompt.
   promptEvent.prompt();
+
   // Log the result
   const result = await promptEvent.userChoice;
   console.log('👍', 'userChoice', result);
+
   // Reset the deferred prompt variable, since
   // prompt() can only be called once.
   window.deferredPrompt = null;
+
   // Hide the install button.
   liInstall.classList.toggle('hidden', true);
 });
